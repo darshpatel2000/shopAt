df9eac7e7e23827f1b1a99c619685548720da9a2
Darsh Patel <darshpatel@Darshs-MacBook-Air.local>
2023-05-15 20:23:16 -0400

    final project with new features resizing image and logout button with csrf validation

---
+++ README.md
@@ -0,0 +1,10 @@
+# shopAt | Django
+
+This is a school project created to learn python and django framework.
+
+## Author
+Team members: Darsh and Simrat
+
+Copyright ©.
+All rights reserved.
+All trademarks, service marks, trade names, and logos referenced herein belong to their respective owners.
---
+++ item/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+    
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    quantity = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100)], default=1)#
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
